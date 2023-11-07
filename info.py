e6487f30850b2d3943521f1f21647a42913e8b64
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-11-07 19:08:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,44 +13,44 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '27402720'))
-API_HASH = environ.get('API_HASH', '38f0682dce493e07863bc6783016e98d')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6270831968:AAF5czwXXtX-cp69YtvBrsmaOdpX8KjzRzo")
+API_ID = int(environ.get('API_ID', '21518327'))
+API_HASH = environ.get('API_HASH', 'e72f588b3e4763f01eecfc3c4aa7e8ac')
+BOT_TOKEN = environ.get('BOT_TOKEN', "6431602729:AAFzVu51ETFyVMdNAc6NsLzLm8LcvHAMAKM")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/75cc0f1fb6b7827e3d73b.jpg https://telegra.ph/file/08d19a064ab11d4e8d5f1.jpg https://telegra.ph/file/c0131fc56681d1e9a6a91.jpg https://telegra.ph/file/31e018771743358431aca.jpg https://telegra.ph/file/c0db1765952409b610e1b.jpg https://telegra.ph/file/bacccd7b9fad5a2cd60d8.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/cb33c25d556d5d2a09861.jpg https://telegra.ph/file/88b07094ef05c500a50e3.jpg https://telegra.ph/file/99be080f697accc77515f.jpg https://telegra.ph/file/32e4dfd942a944bdab1da.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/bacccd7b9fad5a2cd60d8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6032339203 5864846606').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5741918628').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001675304972').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6032339203 5864846606').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5741918628').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '5864846606').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001859923275')
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001859923275')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '--1001901111266')
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001670122897')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ashupatil62611:DH6bdsudNKz9oSgH@cluster0.zqbv07r.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram_filesg")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vinayjaat:vinay123@vinay1.3yzdb9g.mongodb.net/?retryWrites=true&w=majority ")
+DATABASE_NAME = environ.get('DATABASE_NAME', "vinay1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filesg')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'paisakamalo.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '184fd3a38855d49ca0574e06dfe12424493bdd4a')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Vnshortener.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '1ee9140a88d9813183e2af2870487c5eae50e285')
 SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'paisakamalo.in')
 SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '184fd3a38855d49ca0574e06dfe12424493bdd4a')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
@@ -63,8 +63,8 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movies_dekh_01')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/upgrade_botz')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001859923275'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'upgrade_botz')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001839965169'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
