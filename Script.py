e3cce7c03dc394a371b480572891b72f6d808064
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-12-06 21:36:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -245,3 +245,28 @@ Aᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs:
 ░░░██║░░░░░░██║░░░  ██╔══██╗██║░░██║░░░██║░░░░╚═══██╗
 ░░░██║░░░░░░██║░░░  ██████╦╝╚█████╔╝░░░██║░░░██████╔╝
 ░░░╚═╝░░░░░░╚═╝░░░  ╚═════╝░░╚════╝░░░░╚═╝░░░╚═════╝░"""
+
+    PREMIUM_TXT = """<b>Join a Prime membership to get free movie without any ads
+Your time is valuable, Stop watching ads and simply avoid the stupidity
+</a>
+
+Prime Membership Progress
+
+1 month - 40 Rs
+
+3 Months – 100 RS
+
+6 months - 200 Rs
+
+12 Months - 450 Rs [1 Month Free Trial]
+
+♛ UPI ID »»————>  oklink@axl
+Would you like to become a Prime Member? Follow these steps:-
+
+✤ Step 1: Go to any payment app like Google PAY, PAYTM,
+✤ Step 2: Pay using UPI ID depending on your plan.
+✤ Step 3 : Send screenshot to our admin or owner Or contact me to send a screenshot.
+
+send screenshot @V_Sbotmaker
+
+Any doubts or how to become a prime member? contact me @V_Sbotmaker @VS_bots</code></b>"""
