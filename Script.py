7202cc42a7adb74983d95e80dd8da62107e799c4
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-12-06 20:33:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,30 @@ Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
 <b>✯ Uᴘᴅᴀᴛᴇs : <a href=https://t.me/+RJA5879oHfVjZGQ9>🎲Bot updates 🪈</a></b>
 <b>✯ Bᴜɪʟᴅ Sᴛᴀᴛᴜs : ᴠ3.0 [Sᴛᴀʙʟᴇ]</b>"""
 
-    PREMIUM_TXT = """ADD YOUR PREMIUM PLAN ON REPO SCRIPT.PY"""
+        PREMIUM_TXT = """<b>Join a Prime membership to get free movie without any ads
+Your time is valuable, Stop watching ads and simply avoid the stupidity
+</a>
+
+Prime Membership Progress
+
+1 month - 40 Rs
+
+3 Months – 120 RS
+
+6 months - 230 Rs
+
+12 Months - 450 Rs [1 Month Free Trial]
+
+♛ UPI ID »»————>  oklink@axl
+Would you like to become a Prime Member? Follow these steps:-
+
+✤ Step 1: Go to any payment app like Google PAY, PAYTM,
+✤ Step 2: Pay using UPI ID depending on your plan.
+✤ Step 3 : Send screenshot to our admin or owner Or contact me to send a screenshot.
+
+send screenshot @V_Sbotmaker
+
+Any doubts or how to become a prime member? contact me @V_Sbotmaker @VS_bots</code></b>"""
 
     SOURCE_TXT = """
 <b>Tʜɪꜱ Bᴏᴛ Iꜱ NOT Oᴘᴇɴ Sᴏᴜʀᴄᴇ Pʀᴏᴊᴇᴄᴛ.
