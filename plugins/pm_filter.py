a30662a5cae81d14b025c7d3c0a33ed1efb060e2
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2023-12-13 20:37:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1769,7 +1769,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_text(f"<b><i> ⚡ 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+            m=await message.reply_text(f"<b><i> ⚡ ᴀɪ ɪꜱ ꜱᴇᴀʀᴄʜɪɴɢ ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛ... '{search}' 🔎</i></b>")
             search = search.lower()
             find = search.split(" ")
             search = ""
@@ -1800,7 +1800,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_text(f"<b><i> ⚡ 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+        m=await message.reply_text(f"<b><i> ⚡ ᴀɪ ɪꜱ ꜱᴇᴀʀᴄʜɪɴɢ ʏᴏᴜʀ ʀᴇQᴜᴇꜱᴛ... '{search}' 🔎</i></b>")
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
     # if 'is_shortlink' in settings.keys():
